e0a1e96a2852a5f0b499270b88c28dbb9909aeb5
Rima Rajendran M K <90393020+Rima-Rajendran-M-K@users.noreply.github.com>
2021-09-17 00:23:21 +0530

    Update Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -1,56 +1,137 @@
-from random import randint
+#from IPython.display import clear_output
 
-def printboard(bo):
-    print(bo[7]+'|'+bo[8]+'|'+bo[9])
-    print('-|-|-')
-    print(bo[4]+'|'+bo[5]+'|'+bo[6])
-    print('-|-|-')
-    print(bo[1]+'|'+bo[2]+'|'+bo[3])
+
+def display_board(board):
+    #clear_output()  # Remember, this only works in jupyter!
+
+    print('   |   |')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('   |   |')
+
 
 def player_input():
-    choice = ''
-    player1 = 'X'
-    player2 = 'O'
-    while choice != 'X' and choice != 'O':
-        choice = input('Player 1 select (X / O): ').upper()
-        player1 = choice
-    if player1 == 'X':
-        player2 == 'O'
-        return ('X','O')
-    elif player1 == 'O':
-        player2 == 'X'
-        return ('O','X')
-    
-def place_marker(bo, choice, position):
-    bo[position] = choice
-    
-def winner(bo, le):
-    return ((bo[7] == le and bo[8] == le and bo[9] == le) or
-            (bo[4] == le and bo[5] == le and bo[6] == le) or
-            (bo[1] == le and bo[2] == le and bo[3] == le) or
-            (bo[7] == le and bo[4] == le and bo[1] == le) or
-            (bo[8] == le and bo[5] == le and bo[2] == le) or
-            (bo[9] == le and bo[6] == le and bo[3] == le) or
-            (bo[7] == le and bo[5] == le and bo[3] == le) or
-            (bo[9] == le and bo[5] == le and bo[1] == le))
+    marker = ''
 
-def player_position(bo):
+    while not (marker == 'X' or marker == 'O'):
+        marker = input('Player : Do you want to be X or O? ').upper()
+
+    if marker == 'X':
+        return ('X', 'O')
+    else:
+        return ('O', 'X')
+
+def place_marker(board, marker, position):
+    board[position] = marker
+
+
+def win_check(board, mark):
+    return ((board[7] == mark and board[8] == mark and board[9] == mark) or  # across the top
+            (board[4] == mark and board[5] == mark and board[6] == mark) or  # across the middle
+            (board[1] == mark and board[2] == mark and board[3] == mark) or  # across the bottom
+            (board[7] == mark and board[4] == mark and board[1] == mark) or  # down the middle
+            (board[8] == mark and board[5] == mark and board[2] == mark) or  # down the middle
+            (board[9] == mark and board[6] == mark and board[3] == mark) or  # down the right side
+            (board[7] == mark and board[5] == mark and board[3] == mark) or  # diagonal
+            (board[9] == mark and board[5] == mark and board[1] == mark))  # diagonal
+
+import random
+
+def choose_first():
+    if random.randint(0, 1) == 0:
+        return 'Computer '
+    else:
+        return 'Player '
+
+
+def space_check(board, position):
+    return board[position] == ' '
+
+def full_board_check(board):
+    for i in range(1,10):
+        if space_check(board, i):
+            return False
+    return True
+
+
+def player_choice(board):
     position = 0
-    while position not in range(1,10) or not bo[position] == '':
-        position = int(input('Choose next position: '))
 
-def computer_move():
-    return random.randint(0,8)
+    while position not in [1, 2, 3, 4, 5, 6, 7, 8, 9] or not space_check(board, position):
+        position = int(input('Choose your next position: (1-9) '))
 
-print('TIC TAC TOE!')
+    return position
+
+def computer_choice(board):
+    position = 0
+    while position not in [1,2,3,4,5,6,7,8,9] or not space_check(board, position):
+        position = random.randint(1, 9)
+    return position
+
+
+def replay():
+    return input('Do you want to play again? Enter Yes or No: ').lower().startswith('y')
+
+
 while True:
-    Board = ['','','','','','','','','']
-    player1_choice, computer_choice = player_input()
-    round = 1
-    while round < 10:
-        printboard(Board)
-        position = player_position(Board)
-        player_marker(Board, player1_choice)
-        if winner(Board, player1_choice):
-            display_board(Board)
-    round += 1
+    # Reset the board
+    theBoard = [' '] * 10
+    player1_marker, player2_marker = player_input()
+    turn = choose_first()
+    print(turn + ' will go first.')
+
+    play_game = input('Are you ready to play? Enter Yes or No.')
+
+    if play_game.lower()[0] == 'y':
+        game_on = True
+    else:
+        game_on = False
+
+    while game_on:
+        if turn == 'Player ':
+            # Player1's turn.
+
+            display_board(theBoard)
+            position = player_choice(theBoard)
+            place_marker(theBoard, player1_marker, position)
+
+            if win_check(theBoard, player1_marker):
+                display_board(theBoard)
+                print('Congratulations! You have won the game!')
+                game_on = False
+            else:
+                if full_board_check(theBoard):
+                    display_board(theBoard)
+                    print('The game is a draw!')
+                    break
+                else:
+                    turn = 'Computer '
+
+        else:
+            # Player2's turn.
+
+            display_board(theBoard)
+            position =  computer_choice(theBoard)
+            place_marker(theBoard, player2_marker, position)
+
+            if win_check(theBoard, player2_marker):
+                display_board(theBoard)
+                print('Computer has won!')
+                game_on = False
+            else:
+                if full_board_check(theBoard):
+                    display_board(theBoard)
+                    print('The game is a draw!')
+                    break
+                else:
+                    turn = 'Player '
+
+    if not replay():
+        break
