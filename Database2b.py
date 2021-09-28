34ee81e788ab442d52d1ec95767262b415a6acb2
Rima Rajendran M K <90393020+Rima-Rajendran-M-K@users.noreply.github.com>
2021-09-28 23:21:14 +0530

    Update Database2b.py

--- Database2b.py
+++ Database2b.py
@@ -11,28 +11,30 @@ c.execute("""CREATE TABLE DEPARTMENTS (
 print('*****DEPARTMENTS TABLE CREATED*****')
 conn.commit()
 
-department_values = [(2, 'MARKETING'), (2, 'SALES'), (1, 'IT'), (1, 'SALES'), (3, 'IT')]
+department_values = [(5, 'MARKETING'),
+                     (4, 'PURCHASING'),
+                     (1, 'IT'),
+                     (2, 'CLERK'),
+                     (3, 'SALES')]
 c.executemany("INSERT INTO Departments VALUES (?, ?)", department_values)
 print('**DEPARTMENTS TABLE ROWS INSERTED SUCCESSFULLY**')
 
 choice = 1
 while choice:
-    dept_id = input("Enter the department id of your choice: ")
+    dept_id = input("Enter the department id of your choice(1/2/3/4/5): ")
     c.execute("SELECT DEPARTMENT_NAME FROM DEPARTMENTS WHERE DEPARTMENTS_ID = :DEPT_ID", {'DEPT_ID': dept_id})
-    row = c.fetchone()
-    print(f"\nEmployees working in the department {row[0]} are: ")
-    c.execute("SELECT EMP.NAME, EMP.ID, EMP.SALARY, EMP.DEPARTMENT_ID, EMP.CITY, DEPT.DEPARTMENT_NAME "
-              "FROM EMPLOYEE EMP, DEPARTMENTS DEPT where EMP.DEPARTMENT_ID = :DEPT_ID AND DEPT.DEPARTMENTS_ID = :DEPT_ID"
-              , {'DEPT_ID': dept_id})
+    c.execute("SELECT NAME, ID, SALARY, DEPARTMENT_ID, CITY, DEPARTMENT_NAME FROM EMPLOYEE, DEPARTMENTS WHERE "
+              "EMPLOYEE.DEPARTMENT_ID = :DEPT_ID AND DEPARTMENTS.DEPARTMENTS_ID = :DEPT_ID", {'DEPT_ID': dept_id})
     result = c.fetchall()
+    print(f"\nEmployees working in the department {result[0][5]} are: ")
     for i in result:
         print("\nNAME           :", i[0])
         print("ID             :", i[1])
         print("SALARY         :", i[2])
         print("DEPARTMENT_ID  :", i[3])
         print("CITY           :", i[4])
-        print("DEPARTMENT_NAME:", row[1])
-    choice = int(input("Do you wish to continue or not (0/1)"))
+        print("DEPARTMENT_NAME:", i[5])
+    choice = int(input("Do you wish to continue or not (0/1): "))
 
 conn.commit()
 conn.close()
